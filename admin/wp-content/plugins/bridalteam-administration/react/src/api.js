814d48c97c4e310bfca966582160a98f3de98923
Joseph Pender <theproducer@theproducerstudio.com>
2017-11-26 21:46:38 -0600

    Updating react UIs for dev/production

--- admin/wp-content/plugins/bridalteam-administration/react/src/api.js
+++ admin/wp-content/plugins/bridalteam-administration/react/src/api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 export default class Api{
     constructor(){
-        this.apiendpoint = 'http://bridalteam.dev';
+        if(process.env.NODE_ENV == "production"){
+            this.apiendpoint = "http://bridalteam.projectdev.site";
+        }else{
+            this.apiendpoint = "http://bridalteam.dev";
+        }
         
         this.client = null;   
 
--- website/website-react/src/api.js
+++ website/website-react/src/api.js
@@ -4,7 +4,7 @@ export default class Api{
     constructor(){
         this.apiendpoint = "";
         if(process.env.NODE_ENV == "production"){
-            this.apiendpoint = "http://api.bridalteam.com";
+            this.apiendpoint = "http://bridalteam.projectdev.site";
         }else{
             this.apiendpoint = "http://bridalteam.dev";
         }
